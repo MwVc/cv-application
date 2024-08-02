8ac532ad6eb2dcddf7b55269d431ea093224075e
Victor Mwadime <victormwadime@gmail.com>
2024-08-02 14:33:23 +0300

    Create navbar

---
+++ src/components/Nav/Nav.jsx
@@ -0,0 +1,14 @@
+import "./Nav.css";
+
+export default function Nav() {
+  return (
+    <nav className="navbar bg-body-grey">
+      <div className="container">
+        <a href="#" className="navbar-brand fs-2 fw-bolder">
+          <img src="../src/assets/icon.png" alt="logo" width="30" height="24" />
+          CV Application
+        </a>
+      </div>
+    </nav>
+  );
+}
