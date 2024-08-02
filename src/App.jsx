ca37cfb8bf3d1e62031c36949071b9e5daf6992a
Victor Mwadime <victormwadime@gmail.com>
2024-08-02 08:59:00 +0300

    Uninstall bootstrap and sass

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,12 @@ import { useState } from "react";
 import "./App.css";
 
 function App() {
-  return <h1>Hello world</h1>;
+  return (
+    <div className="container">
+      <h1>Hello bootstrap react and vite</h1>
+      <button className="btn btn-primary">Button</button>
+    </div>
+  );
 }
 
 export default App;
