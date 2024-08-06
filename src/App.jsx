c90432230b8c38dc997f368b0680fa56d8b971ae
Victor Mwadime <victormwadime@gmail.com>
2024-08-06 15:39:19 +0300

    create GeneralInformationComponent

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { useState } from "react";
 import "./App.css";
-import { Accordion } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="container" id="cv"></div>;
+  return (
+    <>
+      <div className="container">
+        <div className="container vh-100 d-flex flex-row">
+          <div className="inner-div flex-fill p-3">
+            <section id="general-information">
+              <GeneralInformationForm />
+            </section>
+          </div>
+          <div className="inner-div flex-fill p-3"></div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default App;
