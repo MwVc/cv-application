ddbbe93bcec818b72897194a348747be96448b36
Victor Mwadime <victormwadime@gmail.com>
2024-08-08 13:26:28 +0300

    create and style resume

--- src/App.jsx
+++ src/App.jsx
@@ -13,13 +13,20 @@ function App() {
   return (
     <>
       <div className="container">
-        <div className="container vh-100 d-flex flex-row">
-          <div className="inner-div flex-fill p-3">
+        <div className="container d-flex flex-row">
+          <div className="inner-div flex-fill p-3 w-25">
             <section id="general-information">
               <Form person={person} setPerson={setPerson} />
             </section>
           </div>
-          <div className="inner-div flex-fill p-3"></div>
+          <div className="inner-div flex-fill p-3">
+            <div className="page" data-size="A4">
+              <div className="box">
+                <div className="left-panel"></div>
+                <div className="right-panel"></div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </>
