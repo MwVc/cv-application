b53782e358c76775ed9d746ea71af6918bddfac6
Victor Mwadime <victormwadime@gmail.com>
2024-08-09 17:25:44 +0300

    Add and style resume compoents

--- src/components/Resume/Resume.jsx
+++ src/components/Resume/Resume.jsx
@@ -13,8 +13,25 @@ export default function Resume({ person }) {
               <h2>CONTACT</h2>
               <div className="small-text">
                 <p>
-                  <i className="bi bi-telephone"></i>
-                  {person.firstName}
+                  <i className="bi bi-telephone contact-icon"></i>
+                  +254702150884
+                </p>
+                <p>
+                  <i class="bi bi-envelope contact-icon"></i>
+                  <a
+                    href="https://mail.google.com/mail/u/0/#inbox"
+                    target="_blank"
+                  >
+                    victormwadime@gmail.com
+                  </a>
+                </p>
+                <p>
+                  <i class="bi bi-geo-alt-fill contact-icon"></i>
+                  Mombasa, Kenya
+                </p>
+                <p className="last-pararaph-no-margin-bottom">
+                  <i class="bi bi-linkedin contact-icon"></i>
+                  linkedIn URL
                 </p>
               </div>
             </div>
