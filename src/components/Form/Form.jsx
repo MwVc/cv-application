efdb90a81c735c726c3117cf3dbff91658572988
Victor Mwadime <victormwadime@gmail.com>
2024-08-09 15:04:59 +0300

    refactor bootstrap and react-bootstrap

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,19 +1,41 @@
 import GeneralInformation from "./GeneralInformation";
 import EducationalExperience from "./EducationalExperience";
 import PracticalExperience from "./PracticalExperience";
+import Accordion from "react-bootstrap/Accordion";
 
 export default function Form({ person, setPerson }) {
   return (
-    <div>
-      <div>
-        <GeneralInformation person={person} setPerson={setPerson} />
-      </div>
-      <div>
-        <EducationalExperience />
-      </div>
-      <div>
-        <PracticalExperience />
-      </div>
-    </div>
+    <Accordion defaultActiveKey={["0"]} alwaysOpen>
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>General Information</Accordion.Header>
+        <Accordion.Body>
+          <GeneralInformation />
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="1">
+        <Accordion.Header>Educational Experience</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey="2">
+        <Accordion.Header>Practical Experience</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import Nav from "./components/Nav/Nav.jsx";
+import "bootstrap/dist/css/bootstrap.min.css";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
