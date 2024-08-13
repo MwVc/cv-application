09b719f0cadc0ec7538286ff7d339b681c30dedd
Victor Mwadime <victormwadime@gmail.com>
2024-08-13 14:23:08 +0300

    create EducationalExperience component with input field for institution name and event handler

--- src/components/Form/EducationalExperience.jsx
+++ src/components/Form/EducationalExperience.jsx
@@ -1,3 +1,26 @@
-export default function EducationalExperience() {
-  return <div className="container" style={{ border: "1px solid red" }}></div>;
+import { useState } from "react";
+
+export default function EducationalExperience({ person, setPerson }) {
+  function handleEducation(event) {
+    const { name, value, type } = event.target;
+    console.log(name, value, type);
+  }
+
+  return (
+    <div className="container">
+      <form action="#">
+        <div className="form-group">
+          <label htmlFor="institution">Name of Institution:</label>
+          <input
+            type="text"
+            className="form-control"
+            required={true}
+            onChange={handleEducation}
+            name="institution"
+            id="institution"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
