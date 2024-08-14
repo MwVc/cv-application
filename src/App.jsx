f6e296be4495e070852de6231bbc9b4d7955c804
Victor Mwadime <victormwadime@gmail.com>
2024-08-14 14:14:52 +0300

    delete person and all props

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,17 @@ import Resume from "./components/Resume/Resume";
 import "./App.css";
 
 function App() {
-  const [person, setPerson] = useState({
-    firstName: "",
-    lastName: "",
-    phone: "",
-    email: "",
-    linkedIn: "",
-    city: "",
-    country: "",
-    education: [],
-    workExperience: [],
-    skills: [],
-    hobbies: [],
-  });
-
   return (
     <>
       <div className="container">
         <div className="container d-flex flex-row">
           <div className="inner-div flex-fill p-3 w-25">
             <section id="general-information">
-              <Form person={person} setPerson={setPerson} />
+              <Form />
             </section>
           </div>
           <div className="inner-div flex-fill p-3">
-            <Resume person={person} />
+            <Resume />
           </div>
         </div>
       </div>
