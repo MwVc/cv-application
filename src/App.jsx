8d631c3c8ee2ba7db501e675bad32d3b829e6cdc
Victor Mwadime <victormwadime@gmail.com>
2024-08-14 17:44:49 +0300

    Add state management to App component for handling and passing user input data to Resume component

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,33 @@ import Resume from "./components/Resume/Resume";
 import "./App.css";
 
 function App() {
+  const [generalInformation, setGeneralInformation] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    phone: "",
+    city: "",
+    country: "",
+    profession: "",
+  });
+  const [submittedData, setSubmittedData] = useState(null);
+
   return (
     <>
       <div className="container">
         <div className="container d-flex flex-row">
           <div className="inner-div flex-fill p-3 w-25">
             <section id="general-information">
-              <Form />
+              <Form
+                generalInformation={generalInformation}
+                setGeneralInformation={setGeneralInformation}
+                submittedData={submittedData}
+                setSubmittedData={setSubmittedData}
+              />
             </section>
           </div>
           <div className="inner-div flex-fill p-3">
-            <Resume />
+            <Resume submittedData={submittedData} />
           </div>
         </div>
       </div>
