b7a29c3e12cd4a397df45d19ef9e80c88843b513
Victor Mwadime <victormwadime@gmail.com>
2024-08-15 09:56:27 +0300

    Refactor EducationalExperience component with form submission handling and additional fields

--- src/components/Form/EducationalExperience.jsx
+++ src/components/Form/EducationalExperience.jsx
@@ -1,9 +1,13 @@
 import { useState } from "react";
 
 export default function EducationalExperience() {
+  function handleSubmit() {
+    console.log("I am handling Submit");
+  }
+
   return (
     <div className="container">
-      <form action="#">
+      <form action="#" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="institution">Name of Institution:</label>
           <input
@@ -25,6 +29,38 @@ export default function EducationalExperience() {
             name="qualification"
             id="qualification"
           />
+        </div>
+        <div className="form-group">
+          <label htmlFor="start-year">Start Year</label>
+          <input
+            type="number"
+            className="form-control"
+            required={true}
+            name="startYear"
+            id="start-year"
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="end-year">End Year</label>
+          <input
+            type="number"
+            className="form-control"
+            name="endYear"
+            id="end-year"
+          />
+        </div>
+        <div className="d-flex justify-content-between">
+          <button type="submit" className="btn btn-primary w-45">
+            Submit
+          </button>
+          <button
+            className="btn btn-info w-45"
+            type="button"
+            onClick={console.log("submitting")}
+            disabled={true}
+          >
+            Edit
+          </button>
         </div>
       </form>
     </div>
