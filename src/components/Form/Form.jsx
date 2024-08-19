7a2016283e3d6bc7e088a93ad00379367976fc2a
Victor Mwadime <victormwadime@gmail.com>
2024-08-19 16:43:57 +0300

    Update the Form component to include a section for PracticalExperience, integrate the PracticalExperience component and associated state management properties

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -12,6 +12,10 @@ export default function Form({
   setEducationalExperience,
   submittedEducationalExperience,
   setSubmittedEducationalExperience,
+  practicalExperience,
+  setPracticalExperience,
+  submittedPracticalExperience,
+  setSubmittedPracticalExperience,
 }) {
   return (
     <Accordion defaultActiveKey={["0"]} alwaysOpen>
@@ -42,13 +46,12 @@ export default function Form({
       <Accordion.Item eventKey="2">
         <Accordion.Header>Practical Experience</Accordion.Header>
         <Accordion.Body>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          <PracticalExperience
+            practicalExperience={practicalExperience}
+            setPracticalExperience={setPracticalExperience}
+            submittedPracticalExperience={submittedPracticalExperience}
+            setSubmittedPracticalExperience={setSubmittedPracticalExperience}
+          />
         </Accordion.Body>
       </Accordion.Item>
     </Accordion>
