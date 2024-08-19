ce4ee733d792f4251cfca140fb2d0941afbe8146
Victor Mwadime <victormwadime@gmail.com>
2024-08-19 16:46:02 +0300

    Implement the PracticalExperience component with form handling. Include functions for form submission and input change management

--- src/components/Form/PracticalExperience.jsx
+++ src/components/Form/PracticalExperience.jsx
@@ -1,3 +1,32 @@
-export default function PracticalExperience() {
-  return <div></div>;
+export default function PracticalExperience({
+  practicalExperience,
+  setPracticalExperience,
+  submittedPracticalExperience,
+  setSubmittedPracticalExperience,
+}) {
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submitted");
+  }
+
+  function handleChange(event) {
+    const { value, name } = event.target;
+    setPracticalExperience({ ...practicalExperience, [name]: value });
+    console.log(practicalExperience);
+  }
+  return (
+    <div className="container">
+      <form action="#" method="POST" onSubmit={handleSubmit}>
+        <div className="form-group">
+          <label htmlFor="skill">Skill</label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={handleChange}
+            name="skill"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
