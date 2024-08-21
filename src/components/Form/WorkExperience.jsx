6a5b0ba28c6d49be4d813b145371cd9fe57b4484
Victor Mwadime <victormwadime@gmail.com>
2024-08-21 11:29:23 +0300

    Add start date and end date fields to WorkExperience component

--- src/components/Form/WorkExperience.jsx
+++ src/components/Form/WorkExperience.jsx
@@ -10,6 +10,9 @@ export default function WorkExperience({
   function handleChange() {
     console.log("Handling change");
   }
+  function handleDelete() {
+    console.log("Deleting");
+  }
   return (
     <div className="container">
       <form action="#" method="POST" onSubmit={handleSubmit}>
@@ -50,6 +53,47 @@ export default function WorkExperience({
             required={true}
             value={workExperience.jobDescription}
           ></textarea>
+        </div>
+        <div className="form-group">
+          <label htmlFor="start-date">Satrted:</label>
+          <input
+            type="text"
+            name="startDate"
+            id="start-date"
+            className="form-control"
+            onChange={handleChange}
+            required={true}
+            value={workExperience.startDate}
+          />
+        </div>
+        <div className="form-group">
+          <label htmlFor="end-date">Satrted:</label>
+          <input
+            type="text"
+            name="endDate"
+            id="end-date"
+            className="form-control"
+            onChange={handleChange}
+            required={true}
+            value={workExperience.endDate}
+          />
+        </div>
+        <div className="d-flex justify-content-between">
+          <button
+            type="submit"
+            className="btn btn-primary"
+            disabled={submittedWorkExperience.length === 3}
+          >
+            Submit
+          </button>
+          <button
+            type="button"
+            className="btn btn-info"
+            onClick={handleDelete}
+            disabled={submittedWorkExperience.length === 0}
+          >
+            Delete
+          </button>
         </div>
       </form>
     </div>
