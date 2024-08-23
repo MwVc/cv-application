b71147e8563c48d1600385cc768b7b26f65dc9e9
Victor Mwadime <victormwadime@gmail.com>
2024-08-23 11:10:58 +0300

    Implement project form editing and deletion in Projects component with state management

--- src/components/Form/Projects.jsx
+++ src/components/Form/Projects.jsx
@@ -1,12 +1,23 @@
-export default function Projects() {
+export default function Projects({
+  project,
+  setProject,
+  submittedProjects,
+  setSubmittedProjects,
+}) {
   function handleSubmit(event) {
     event.preventDefault();
-    console.log("Submitting the form...");
   }
+
   function handleChange(event) {
     const { name, value } = event.target;
-    console.log(name, value);
+
+    setProject({ ...project, [name]: value });
   }
+
+  function handleDelete() {
+    console.log("deleting");
+  }
+
   return (
     <div className="container">
       <form action="#" onSubmit={handleSubmit} method="POST">
@@ -34,6 +45,23 @@ export default function Projects() {
             placeholder='e.g In this groundbreaking project, I boldly explored the depths of human-computer interaction by teaching a machine to greet the world. Yes, I taught it to say, "Hello, World!" This involved complex tasks like mastering the syntax of a programming language and strategically placing parentheses and semicolons. The project was a resounding success, with the program greeting the world flawlessly every time it was run.'
             rows={9}
           ></textarea>
+        </div>
+        <div className="d-flex justify-content-between">
+          <button
+            type="submit"
+            className="btn btn-primary w-45"
+            disabled={submittedProjects.length === 3}
+          >
+            Submit
+          </button>
+          <button
+            className="btn btn-info w-45"
+            type="button"
+            disabled={submittedProjects.length === 0}
+            onClick={handleDelete}
+          >
+            Delete
+          </button>
         </div>
       </form>
     </div>
