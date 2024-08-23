8aefb3ddaca2b357853fd1bea6b75655856cbd86
Victor Mwadime <victormwadime@gmail.com>
2024-08-23 10:49:22 +0300

    Implement project form in Projects component with name input and handling

--- src/components/Form/Projects.jsx
+++ src/components/Form/Projects.jsx
@@ -1,3 +1,28 @@
 export default function Projects() {
-  return <div className="container"></div>;
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log("Submitting the form...");
+  }
+  function handleChange(event) {
+    const { name, value } = event.target;
+    console.log(name, value);
+  }
+  return (
+    <div className="container">
+      <form action="#" onSubmit={handleSubmit} method="POST">
+        <div className="form-group">
+          <label htmlFor="project-name">Project Name:</label>
+          <input
+            type="text"
+            id="project-name"
+            name="projectName"
+            className="form-control"
+            required={true}
+            onChange={handleChange}
+            placeholder="e.g Hello World"
+          />
+        </div>
+      </form>
+    </div>
+  );
 }
