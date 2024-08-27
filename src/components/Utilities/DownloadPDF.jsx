4c0e1bde3639f36a88be3ff04c246084bc1719de
Victor Mwadime <victormwadime@gmail.com>
2024-08-27 09:59:31 +0300

    Implement PDF download functionality for resume

--- src/components/Utilities/DownloadPDF.jsx
+++ src/components/Utilities/DownloadPDF.jsx
@@ -1,4 +1,22 @@
+import { html2pdf } from "html2pdf.js";
+
 export default function DownloadPDF() {
+  const element = document.querySelector(".page");
+  const pdfOptions = {
+    margin: -7,
+    filename: "my-cv.pdf",
+    image: {
+      type: "jpeg",
+      quality: 1,
+    },
+    html2canvas: {
+      scale: 4,
+      dpi: 300,
+      letterRendering: true,
+      useCORS: true,
+    },
+    jsPDF: { unit: "mm", format: "a4", oreintation: "potrait" },
+  };
   return (
     <div>
       <button>Download CV</button>
