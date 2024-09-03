9b38a3e8d116b97285b43c4c4b86e390221994e1
Victor Mwadime <victormwadime@gmail.com>
2024-09-03 16:40:35 +0300

    Move icon.png to public folder

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -5,7 +5,7 @@ export default function Nav() {
     <nav className="navbar bg-body-grey">
       <div className="container">
         <a href="#" className="navbar-brand fs-2 fw-bolder">
-          <img src="../src/assets/icon.png" alt="logo" width="30" height="24" />
+          <img src="/icon.png" alt="logo" width="30" height="24" />
           CV Application
         </a>
       </div>
