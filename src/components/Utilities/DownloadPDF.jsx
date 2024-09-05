7d63eb4f054d8021bb46d012e5de4e90bb1e7e03
Victor Mwadime <victormwadime@gmail.com>
2024-09-05 10:08:58 +0300

    delete options in generatePDF funtion

--- src/components/Utilities/DownloadPDF.jsx
+++ src/components/Utilities/DownloadPDF.jsx
@@ -1,26 +1,10 @@
-import html2pdf from "html2pdf.js";
+import { jsPDF } from "jspdf";
 
 function generatePDF() {
   const element = document.querySelector(".page");
-  const options = {
-    margin: -0.1,
-    filename: "my-cv.pdf",
-    image: {
-      type: "jpeg",
-      quality: 1,
-    },
-    html2canvas: {
-      scale: 4,
-      dpi: 300,
-      letterRendering: true,
-      useCORS: true,
-    },
-    jsPDF: { unit: "mm", format: "a4", orientation: "portrait" },
-  };
-  html2pdf().set(options).from(element).save();
 }
 
-export default function DownloadPDF() {
+function DownloadPDF() {
   return (
     <div>
       <button
