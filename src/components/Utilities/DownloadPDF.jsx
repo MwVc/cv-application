eb16aef405dac7beda680f097f629ebfcbf8ded7
Victor Mwadime <victormwadime@gmail.com>
2024-10-16 13:41:40 +0300

    export DownloadPDF component

--- src/components/Utilities/DownloadPDF.jsx
+++ src/components/Utilities/DownloadPDF.jsx
@@ -1,10 +1,19 @@
-import { jsPDF } from "jspdf";
+import html2pdf from "html2pdf.js";
 
 function generatePDF() {
   const element = document.querySelector(".page");
+  const opt = {
+    margin: -0.0017,
+    filename: "myfile.pdf",
+    image: { type: "jpeg", quality: 0.98 },
+    html2canvas: { scale: 2 },
+    jsPDF: { unit: "in", format: "a4", orientation: "portrait" },
+  };
+
+  html2pdf().set(opt).from(element).save();
 }
 
-function DownloadPDF() {
+export default function DownloadPDF() {
   return (
     <div>
       <button
